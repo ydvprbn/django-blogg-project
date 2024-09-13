e5ce37231f74a1c391d0523b8197253d18e2040d
Prabin Yadav <ydprabin11@gmail.com>
2024-09-13 12:08:02 +0545

    initial commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
-MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
