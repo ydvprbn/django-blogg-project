62bda91dd5fae8c2ba83519a935e02c10ba93a4e
ydvprbn <prabin.ydv098@gmail.com>
2025-01-28 01:11:11 +0545

    updated urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.conf import settings
+from . import settings
 from users import views as user_views
 
 urlpatterns = [
@@ -61,6 +61,5 @@ urlpatterns = [
     path("", include("blog.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
